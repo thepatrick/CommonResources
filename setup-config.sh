36c5f1d082e29aaa90147dabc156d184b853ddd4
Patrick Quinn-Graham <make-contact@pftqg.com>
2013-11-16 19:05:39 +1100

    Add setup configurator

---
+++ setup-config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s `pwd`/config/ackrc ~/.ackrc
+ln -s `pwd`/config/bash_login ~/.bash_login
+ln -s `pwd`/config/bashrc ~/.bashrc
+ln -s `pwd`/config/git-completion.sh ~/.git-completion.sh
+ln -s `pwd`/config/inputrc ~/.inputrc
+ln -s `pwd`/config/ssh/config ~/.ssh/config
