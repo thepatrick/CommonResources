edc2784f970ba5e9e4a850e8c70112bdcc59dd7b
Patrick Quinn-Graham <make-contact@pftqg.com>
2016-01-04 19:52:46 +1100

    Add .vim folder & init submodules so it'll be usable

--- setup-config.sh
+++ setup-config.sh
@@ -7,3 +7,7 @@ ln -s `pwd`/config/git-completion.sh ~/.git-completion.sh
 ln -s `pwd`/config/inputrc ~/.inputrc
 ln -s `pwd`/config/ssh/config ~/.ssh/config
 ln -s `pwd`/config/vimrc ~/.vimrc
+ln -s `pwd`/config/vim ~/.vim
+
+git submodule init
+git submodule update
