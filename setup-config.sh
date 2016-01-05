e481b6d0984a71a96c9fd54f148044017e7df8a7
Patrick Quinn-Graham <make-contact@pftqg.com>
2016-01-05 14:01:56 +1100

    Make setup-config.sh smarter (only make links that don't exist yet)

--- setup-config.sh
+++ setup-config.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-ln -s `pwd`/config/ackrc ~/.ackrc
-ln -s `pwd`/config/bash_login ~/.bash_login
-ln -s `pwd`/config/bashrc ~/.bashrc
-ln -s `pwd`/config/git-completion.sh ~/.git-completion.sh
-ln -s `pwd`/config/inputrc ~/.inputrc
-ln -s `pwd`/config/ssh/config ~/.ssh/config
-ln -s `pwd`/config/vimrc ~/.vimrc
-ln -s `pwd`/vim ~/.vim
+function setup_link() {
+  if [ -h $2 ]; then
+    if [ `readlink $2` != "$1" ]; then
+      echo $2 -h exists but points to `readlink $2`, remove it and run $0 again.
+    fi
+  elif [ -d $2 ]; then
+    echo $2 -d exists, move it aside and run $0 again.
+	elif [ -f $2 ]; then
+    echo $2 -f exists, move it aside and run $0 again.
+  else
+    echo $2 linking to $1
+    ln -s "$1" "$2"
+	fi
+}
+
+setup_link `pwd`/config/ackrc ~/.ackrc
+setup_link `pwd`/config/bash_login ~/.bash_login
+setup_link `pwd`/config/bashrc ~/.bashrc
+setup_link `pwd`/config/git-completion.sh ~/.git-completion.sh
+setup_link `pwd`/config/inputrc ~/.inputrc
+setup_link `pwd`/config/ssh/config ~/.ssh/config
+setup_link `pwd`/config/vimrc ~/.vimrc
+setup_link `pwd`/vim ~/.vim
 
 git submodule init
 git submodule update
