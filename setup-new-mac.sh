99fc57c72b5cd422ce16f4f0a42e7d22280c6c41
Patrick Quinn-Graham <make-contact@pftqg.com>
2019-06-07 23:41:22 +1000

    Add some more setup stuff on a mac

--- setup-new-mac.sh
+++ setup-new-mac.sh
@@ -12,11 +12,7 @@ pushd $HOME/Developer/CommonResources
 # load yubikeys.asc in to gpg (put this in this repo?)
 # install safari u2f extension: https://github.com/Safari-FIDO-U2F/Safari-FIDO-U2F
 
-# brew cask install hyper\
-
 # brew tap homebrew/cask-versions
-#   
-
 
 # brew install https://raw.githubusercontent.com/shtirlic/yubikeylockd/master/yubikeylockd.rb
 # ... and then: sudo brew services start yubikeylockd
@@ -24,9 +20,19 @@ pushd $HOME/Developer/CommonResources
 # git remote rm origin
 # git remote add origin git@github.com:thepatrick/CommonResources.git
 
-# Copy keybase challenges:
-# cp /keybase/private/thepatrick/yubico/challenge-* ~/.yubico/
-# Add to /etc/pam.d/screensaver after "account required pam_opendirectory.so"
-#   auth       required       /usr/local/lib/security/pam_yubico.so mode=challenge-response
-# Add to /etc/pam.d/authorization after "account required pam_opendirectory.so"
-#   auth       required       /usr/local/lib/security/pam_yubico.so mode=challenge-response
+# Pair required smart cards
+# install RequireSmartcard.mobileconfig
+
+# sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+# sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
+# sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+# sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned off
+# sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
+# if changed any of the above:
+# sudo pkill -HUP socketfilterfw
+# brew install lulu
+# brew cask install bitbar
+# brew install dnscrypt-proxy
+# sudo brew services start dnscrypt-proxy
+# bitbar://openPlugin?title=dnscrypt-proxy%20switcher&src=https://github.com/matryer/bitbar-plugins/raw/master/Network%2fdnscrypt-proxy-switcher.10s.sh
+# bitbar://openPlugin?title=Brew%20Services&src=https://github.com/matryer/bitbar-plugins/raw/master/Dev%2fHomebrew%2fbrew-services.10m.rb
